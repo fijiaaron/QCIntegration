d496d83a1c28f3ae57639bfa75a5bc4ad6f0aac7
Aaron Evans <aarone@one-shore.com>
2011-11-28 21:09:39 +0000

    simplified test base and removed print statements

--- java/src/oneshore/example/qcintegration/TestBase.java
+++ java/src/oneshore/example/qcintegration/TestBase.java
@@ -1,8 +1,6 @@
 package oneshore.example.qcintegration;
 
 import java.lang.reflect.Method;
-import java.util.ArrayList;
-import java.util.List;
 
 import org.junit.After;
 import org.junit.Before;
@@ -10,51 +8,18 @@ import org.junit.Rule;
 import org.junit.rules.TestName;
 
 public class TestBase {
-
-	List<String> covered;
-	List<String> related;
-	
 	@Rule public TestName testName = new TestName();
+	
+	QCTestCases qcTestCases;
 	
 	@Before
 	public void getQCTestCoverage() {
-		print("in getQCTestCoverage()");
-		
-		
-		print("testName: " + testName.getMethodName());
-		print("reflected class name: " + this.getClass().getCanonicalName());
 		try {
 			Method m = this.getClass().getMethod("testSomething");
-			print("reflected method name: " + m.getName());
 			
 			if (m.isAnnotationPresent(QCTestCases.class)) {
-				print("found annotation");
-				QCTestCases qcTestCases = m.getAnnotation(QCTestCases.class);
-				covered = new ArrayList<String>();
-				related = new ArrayList<String>();
-				
-				for (String coveredTestId : qcTestCases.covered())
-				{
-					print("covered test: " + coveredTestId);
-					if (covered == null) {
-						covered = new ArrayList<String>();
-					}		
-					covered.add(coveredTestId);
-				}
-				for (String relatedTestId : qcTestCases.related())
-				{
-					print("related test: " + relatedTestId);
-					
-					if (related == null) {
-						related = new ArrayList<String>();
-					}
-					related.add(relatedTestId);
-				}
-
-			} else {
-				print("no annotation found");
-			}
-			
+				qcTestCases = m.getAnnotation(QCTestCases.class);
+			} 
 		} catch (SecurityException e) {
 			e.printStackTrace();
 		} catch (NoSuchMethodException e) {
@@ -67,17 +32,13 @@ public class TestBase {
 		StringBuilder result = new StringBuilder();
 		result.append(testName.getMethodName());
 		result.append(",");
-		for (String qcTestId : covered)
+		
+		for (String qcTestId : qcTestCases.covered())
 		{
 			result.append(qcTestId);
 			result.append(",");
 		}
 		
-		print("qc tests covered: " + result);
-		
-	}
-	
-	public static void print(String s) {
-		System.out.println(s);
+		System.out.println("qc tests covered: " + result);
 	}
 }
