57b3898fd73a988686404ad41173d873437e0df9
Aaron Evans <aarone@one-shore.com>
2011-11-28 21:22:44 +0000

    spacing cleanup

--- java/src/oneshore/example/qcintegration/QCTestCases.java
+++ java/src/oneshore/example/qcintegration/QCTestCases.java
@@ -8,7 +8,6 @@ import java.lang.annotation.Target;
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.METHOD)
 public @interface QCTestCases {
-	
 	String[] covered();
 	String[] related();
 }
