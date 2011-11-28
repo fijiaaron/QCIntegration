12792dc762f2f8fb9b0e62a3ca7ef6d3d2646c2e
Aaron Evans <aarone@one-shore.com>
2011-11-28 21:10:13 +0000

    removed comments from MyTest

--- java/test/oneshore/example/qcintegration/MyTest.java
+++ java/test/oneshore/example/qcintegration/MyTest.java
@@ -8,7 +8,6 @@ public class MyTest extends TestBase {
 	@Test
 	@QCTestCases(covered={"QC-TEST-1", "QC-TEST-2"}, related={"QC-TEST-3"})
 	public void testSomething() {
-		print("in testSomething()");
-		assertTrue(false);
+		assertTrue(true);
 	}
 }
