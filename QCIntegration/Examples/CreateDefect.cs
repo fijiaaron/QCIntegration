9390f441c175131207517801fbdb9e75fc0e17ad
Aaron Evans <aarone@one-shore.com>
2012-11-20 15:16:09 -0700

    added examples for creating defects and running tests in QualityCenter via the OTA API.

---
+++ QCIntegration/Examples/CreateDefect.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using TDAPIOLELib;
+
+namespace oneshore.QCIntegration.Examples
+{
+    class CreateDefect
+    {
+        public CreateDefect()
+        {
+            TDConnection connection = Connect("qcUrl", "qcDomain", "qcProject", "qcLoginName", "qcPassword");
+
+            BugFactory bugFactory = connection.BugFactory;
+            Bug bug = bugFactory.AddItem(null);
+
+            bug.Status = "New";
+            bug.Project = "QCIntegration";
+            bug.Summary = "Short description of the bug";
+            bug.DetectedBy = "Aaron Evans";
+            bug.AssignedTo = "Nobody";
+            bug.Priority = "Low";
+     
+            bug.Post();
+        }
+
+        public TDConnection Connect(string qcUrl, string qcDomain, string qcProject, string qcLoginName, string qcPassword)
+        {
+            var connection = new TDConnection();
+            connection.InitConnectionEx(qcUrl);
+            connection.ConnectProjectEx(qcDomain, qcProject, qcLoginName, qcPassword);
+
+            return connection;
+        }
+    }
+}
---
+++ QCIntegration/Examples/RunTests.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using TDAPIOLELib;
+
+namespace oneshore.QCIntegration.examples
+{
+    public class RunTests
+    {
+        public String qcUrl = "http://localhost:8080/qcbin";
+        public String qcDomain = "oneshore";
+        public String qcProject = "QCIntegration";
+        public String qcLoginName = "aaron";
+        public String qcPassword = "secret";
+
+        public String testSetPath = @"ROOT\path\to\testset";
+        public String testSetName = "TestSet name";
+
+        private TDConnection connection;
+
+        public RunTests()
+        {
+            Connect(qcUrl, qcDomain, qcProject, qcLoginName, qcPassword);
+            TestSet testSet = GetTestSet(testSetPath, testSetName);
+            RunTestSet(testSet);
+        }
+
+        public TDConnection Connect(string qcUrl, string qcDomain, string qcProject, string qcLoginName, string qcPassword)
+        {
+            connection = new TDConnection();
+            connection.InitConnectionEx(qcUrl);
+            connection.ConnectProjectEx(qcDomain, qcProject, qcLoginName, qcPassword);
+
+            return connection;
+        }
+
+        public TestSet GetTestSet(String path, String testSetName)
+        {
+            TestSetFactory testSetFactory = connection.TestSetFactory;
+            TestSetTreeManager testSetTreeManager = connection.TestSetTreeManager;
+
+            TestSetFolder testSetFolder = (TestSetFolder)testSetTreeManager.NodeByPath[path];
+            List testSetList = testSetFolder.FindTestSets(testSetName);
+            TestSet testSet = testSetList[0];
+
+            return testSet;
+        }
+
+        public void RunTestSet(TestSet testSet)
+        {
+            TSScheduler scheduler = testSet.StartExecution("");
+            scheduler.RunAllLocally = true;
+            scheduler.Run();
+        }
+    }
+}
